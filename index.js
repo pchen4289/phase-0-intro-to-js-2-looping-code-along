ad7e88188dce6be737994a7b80123bc1258b228f
Patty Chen <patty.chen1017@gmail.com>
2022-04-15 18:22:28 -0400

    commit tree 300b6971a645c48c7d247b1440c3858314852835

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const recipients = ["Guadalupe","Ollie","Aki"];
+const event = 'surprise';
+function writeCards(recipients, event) {
+    let thankYouLetters = []
+    for (let i = 0; i < recipients.length; i++) {
+    thankYouLetters.push(`Thank you, ${recipients[i]}, for the wonderful ${event} gift!`);
+    }
+return thankYouLetters;
+}
+
+function countDown(numbers){
+while (numbers >= 0){
+    console.log(numbers);
+    numbers--;}
+}
